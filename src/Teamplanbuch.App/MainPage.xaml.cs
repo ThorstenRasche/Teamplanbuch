ea85735270139e40c3c0b99e165d70ab68afb2ca
Rasche0809 <90986838+Rasche0809@users.noreply.github.com>
2022-12-15 22:54:06 +0100

    added app

---
+++ src/Teamplanbuch.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Teamplanbuch.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Teamplanbuch.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Teamplanbuch.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
